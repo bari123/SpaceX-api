f24e6f8a37b4911c43c45851d1a79de065581f04
bari123 <42273534+bari123@users.noreply.github.com>
2021-12-11 17:32:34 +0100

    Add files via upload

---
+++ src/results.js
@@ -0,0 +1,56 @@
+import { LoadMission1 } from "./queries/query";
+import React from 'react';
+import { useQuery } from "@apollo/client";
+import { Box, Typography,CardMedia } from "@material-ui/core";
+import { Modal } from '@mui/material';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+const UseLazyGetQuery = ({ closeModal, datas1 }) => {
+
+    var launch = datas1;
+    console.log(datas1);
+    const { loading, error, data } = useQuery(LoadMission1, {
+        variables: { launch }
+    });
+
+
+    if (loading) { return <div><p>Loading...</p></div>; }
+    if (error) { return <div><p>eror</p></div> }
+
+    return (
+
+        <div>
+
+            <Modal
+                open={true}
+                onClose={() => closeModal(false)}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <div >
+                    {data.launchesPast.map(({ launch_site,mission_name,id,ships }) => (
+                        <Box sx={style} key={id} >
+                            <Typography id="modal-modal-title" variant="h6" component="h2">{mission_name}</Typography>
+                            <Typography id="modal-modal-description" sx={{ mt: 2 }}>{launch_site.site_name}</Typography>
+                            <CardMedia component="img" height="100" width="100" image={ships.image} alt={ships.id}/>
+                        </Box>
+                    ))
+                    }
+                </div>
+            </Modal>
+        </div>
+    )
+}
+
+export default UseLazyGetQuery;
